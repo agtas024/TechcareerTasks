db73f886750b88363edc4139d62fa2f9e93c658f
Muammer Agtas <programuammer@gmail.com>
2022-03-30 18:30:06 +0300

    Techcareer

---
+++ app/src/main/java/com/example/tasks/techcareertasks/task_1/Task_1.java
@@ -0,0 +1,29 @@
+package com.example.tasks.techcareertasks.task_1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+        //Değerler örnek olarak girilmiştir.
+
+        int ogrno = 1923;
+        int yazarno = 1;
+        int turno = 1;
+        int kitapno = 1;
+        int isbno = 1;
+        int sayfasayisi = 543;
+        int puan = 98;
+        int islemno = 1;
+        byte sinif = 11;
+        char cinsiyet = 'E';
+        String yazarad = "Mustafa Kemal";
+        String yazarsoyad = "Atatürk";
+        String turadi = "Söylev";
+        String kitapadi = "Nutuk";
+        String atarih = "01/01/2022";
+        String vtarih = "01/02/2022";
+        String ograd = "Ali";
+        String ogrsoyad = "Akın";
+        String dtarih = "01/01/2005";
+
+    }
+}
